457ec842e25a680cbebd8181c614a218de9e5c48
rauf-alluvium <intern@novusha.com>
2025-08-14 19:01:20 +0530

    elock gps

--- client/src/components/ElockGPSOperation.jsx
+++ client/src/components/ElockGPSOperation.jsx
@@ -334,6 +334,7 @@ const ElockGPSOperation = ({ isOpen, onClose, elockNo }) => {
   const ADMIN_API_URL = "http://icloud.assetscontrols.com:8092/OpenApi/Admin";
   const LBS_API_URL = "http://icloud.assetscontrols.com:8092/OpenApi/LBS";
   const SERVER_URL = import.meta.env.VITE_API_BASE_URL || "http://15.207.11.214:5004/api";
+
 // const SERVER_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5004/api";
   // Real-time tracking functions
   const connectToRealTimeTracking = useCallback(() => {
@@ -1283,7 +1284,8 @@ const ElockGPSOperation = ({ isOpen, onClose, elockNo }) => {
                             variant="h6"
                             sx={{ color: "secondary.main", fontWeight: "bold" }}
                           >
-                            {assetData.FAgentName}
+                            {/* //{assetData.FAgentName} */}
+                            Novusha Consulting Services India LLP
                           </Typography>
                         </Box>
                       </Grid>
